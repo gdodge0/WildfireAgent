a1da9d385bd32b2e9c821f73cc76241cd4708be8
Evan <evanshahrestany3@gmail.com>
2024-10-18 23:14:29 -0700

    hiiii

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return '<p>Hello World</p>'
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
