c76f2b8315bc4dc53dc99f7cd7a6381160f150b7
sradisay <sradisay@gmail.com>
2024-10-18 21:23:31 -0700

    created frontend vue.js server
    - added tailwind.css
    - "npm run dev" to run frontend server

---
+++ wildfireagent_frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <span class="text-red-500 font-medium">tester</span>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+
