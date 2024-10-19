6dcd3ffd0819b778b6728608ee2b814ecaa924de
Evan <evanshahrestany3@gmail.com>
2024-10-19 12:06:59 -0700

    implement vue router, locationUtils

---
+++ wildfireagent_frontend/src/components/GoogleMap.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { GoogleMap, Marker } from 'vue3-google-map'
+import { getCurrentInstance } from 'vue'
+
+// Access global properties using getCurrentInstance()
+const instance = getCurrentInstance()
+const gmaps_api_key = instance.appContext.config.globalProperties.$gmaps_api_key
+
+const center = { lat: 37.784336, lng: -122.4059412 }
+</script>
+
+<template>
+  <GoogleMap
+    :api-key="gmaps_api_key"
+    style="width: 80%; height: 500px"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+
+<style scoped>
+.map-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
