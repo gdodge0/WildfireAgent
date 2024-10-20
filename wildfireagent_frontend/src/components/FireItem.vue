b93a8bb68855abdbb0beb323578f8a61e1dade17
Evan <evanshahrestany3@gmail.com>
2024-10-20 01:06:03 -0700

    latest info timestamping and clamping

--- wildfireagent_frontend/src/components/FireItem.vue
+++ wildfireagent_frontend/src/components/FireItem.vue
@@ -25,6 +25,11 @@ defineProps({
     type: String,
     required: true
   },
+  news_timestamp: {
+    type: String,
+    required: true,
+    default: "Loading..."
+  },
   id: {
     type: String,
     required: true
@@ -37,7 +42,7 @@ defineProps({
   <div class="p-4 bg-neutral-900 flex flex-col justify-stretch items-stretch text-gray-300 border border-white rounded-2xl">
     <div class="flex flex-row justify-between items-start text-white text-lg font-medium">
       <div>
-        <h2>{{ name }}</h2>
+        <h2 class="line-clamp-1">{{ name }}</h2>
         <div class="flex flex-row justify-between items-center text-gray-500 text-sm">
           <h2>{{ Math.floor(proximity) }} miles away</h2>
         </div>
@@ -56,11 +61,11 @@ defineProps({
       <span>Size: <span class="text-green-500">{{ size === null ? "Unknown" : size + " acres"}}</span></span>
     </div>
     <div class="flex flex-row justify-between items-center gap-x-4">
-      <div class="flex flex-col justify-start">
-        <span>Latest Info:</span>
-        <p class="line-clamp-3 ">{{ news }}</p>
-      </div>
+      <div class="flex flex-row items-center justify-start">
+        <span>Latest Info:</span><span class="ml-2 text-gray-500 text-sm">{{ news_timestamp }}</span>
+        </div>
     </div>
+    <p class="line-clamp-3 ">{{ news }}</p>
   </div>
 </template>
 
